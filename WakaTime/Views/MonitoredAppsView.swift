6a9faa65a3685a5709f43c5c6159f0aaf4b076cd
starbugs <mail@tobiaslensing.com>
2024-03-22 11:09:32 +0100

    Attempt to modify stack view to scroll properly (not happy with result)

--- WakaTime/Views/MonitoredAppsView.swift
+++ WakaTime/Views/MonitoredAppsView.swift
@@ -4,18 +4,33 @@ class MonitoredAppsView: NSView {
     init() {
         super.init(frame: .zero)
 
+        let scrollView = NSScrollView(frame: .zero)
+        scrollView.hasVerticalScroller = true
+        scrollView.drawsBackground = false
+
         let stackView = NSStackView(frame: .zero)
         stackView.orientation = .vertical
         stackView.distribution = .equalSpacing
         stackView.alignment = .leading
+        stackView.spacing = 10
 
-        addSubview(stackView)
+        scrollView.documentView = stackView
+
+        addSubview(scrollView)
+
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
+        scrollView.topAnchor.constraint(equalTo: topAnchor, constant: 0).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
 
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32).isActive = true
-        stackView.topAnchor.constraint(equalTo: topAnchor, constant: 32).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -32).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(
+            equalTo: scrollView.trailingAnchor,
+            constant: 0
+        ).isActive = true
+        stackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
 
         buildView(stackView: stackView)
     }
@@ -25,6 +40,14 @@ class MonitoredAppsView: NSView {
     }
 
     func buildView(stackView: NSStackView) {
+        // Transparent spacer view for padding at the top
+        let topSpacerView = NSView()
+        topSpacerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topSpacerView.heightAnchor.constraint(equalToConstant: 10)
+        ])
+        stackView.addArrangedSubview(topSpacerView)
+
         for (index, bundleId) in MonitoredApp.allBundleIds.enumerated() {
             guard !MonitoredApp.unsupportedAppIds.contains(bundleId) else { continue }
 
@@ -35,7 +58,13 @@ class MonitoredAppsView: NSView {
                 bundleId: bundleId.appending("-setapp"))
         }
 
-        stackView.addArrangedSubview(NSView())
+        // Transparent spacer view for padding at the bottom
+        let bottomSpacerView = NSView()
+        bottomSpacerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bottomSpacerView.heightAnchor.constraint(equalToConstant: 10)
+        ])
+        stackView.addArrangedSubview(bottomSpacerView)
     }
 
     func buildViewForApp(index: Int, stackView: NSStackView, bundleId: String) {
@@ -46,9 +75,21 @@ class MonitoredAppsView: NSView {
 
         let currentStackView = NSStackView(frame: .zero)
         currentStackView.orientation = .horizontal
-        currentStackView.distribution = .gravityAreas
+        currentStackView.distribution = .fill
 
-        currentStackView.spacing = 32
+        let paddingLeading = NSView()
+        paddingLeading.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            paddingLeading.widthAnchor.constraint(equalToConstant: 20) // Left padding
+        ])
+
+        let paddingTrailing = NSView()
+        paddingTrailing.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            paddingTrailing.widthAnchor.constraint(equalToConstant: 20) // Right padding
+        ])
+
+        currentStackView.addArrangedSubview(paddingLeading)
 
         let imageView = NSImageView(frame: NSRect(x: 0, y: 0, width: 15, height: 15))
         imageView.image = image
@@ -67,11 +108,12 @@ class MonitoredAppsView: NSView {
         currentStackView.addArrangedSubview(nameLabel)
         currentStackView.addArrangedSubview(switchControl)
 
+        currentStackView.addArrangedSubview(paddingTrailing)
+
         stackView.addArrangedSubview(currentStackView)
         currentStackView.translatesAutoresizingMaskIntoConstraints = false
         currentStackView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor).isActive = true
         currentStackView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor).isActive = true
-        currentStackView.huggingPriority(for: .horizontal)
         nameLabel.widthAnchor.constraint(equalTo: currentStackView.widthAnchor, multiplier: 0.7, constant: 0).isActive = true
 
         let divider = NSView(frame: NSRect(x: 0, y: 0, width: stackView.frame.width, height: 1))
